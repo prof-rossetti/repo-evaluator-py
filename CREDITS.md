b6d4f17fc66cd16122cd3556ea2b45a07a372d5f
Mike Rossetti <s2t2@users.noreply.github.com>
2019-02-18 01:27:09 -0500

    WIP - prepare to consolodate commits by username

--- CREDITS.md
+++ CREDITS.md
@@ -3,3 +3,7 @@
 ## Pytest Configuration
 
   + https://stackoverflow.com/a/50610630/670433
+
+## Counting Commits
+
+  + https://stackoverflow.com/questions/677436/how-do-i-get-the-git-commit-count
--- app/history_checker.py
+++ app/history_checker.py
@@ -12,6 +12,15 @@ def read_excluded_authors(filename="db/authors_excluded.csv"):
             author_names.append(row["author_name"])
     return author_names
 
+def read_author_names(filename="db/user_author_names.csv"):
+    csv_filepath = os.path.join(os.path.dirname(__file__), "..", filename)
+    author_names = []
+    with open(csv_filepath, "r") as csv_file:
+        reader = csv.DictReader(csv_file, skipinitialspace=True) # source: https://stackoverflow.com/a/17255790/670433
+        for row in reader:
+            author_names.append(dict(row))
+    return author_names
+
 def write_results_to_file(results=[], filename="db/histories_checked.csv"):
     csv_filepath = os.path.join(os.path.dirname(__file__), "..", filename)
     headers = list(results[0].keys())
@@ -21,62 +30,81 @@ def write_results_to_file(results=[], filename="db/histories_checked.csv"):
         for result in results:
             writer.writerow(result)
 
+def group_by_username(author_histories):
+    user_histories = []
+    return user_histories
 
+def lookup_username(author_name, users_authors):
+    matching_usernames = [d["username"] for d in users_authors if d["author_name"].upper() == author_name.upper()]
+    try:
+        return matching_usernames[0]
+    except IndexError as err:
+        return author_name
 
 if __name__ == "__main__":
 
+    print("-----------------")
+    print("CHECKING EXCLUDED AUTHORS...")
+    print("-----------------")
 
     excluded_author_names = read_excluded_authors()
+    print(excluded_author_names)
+
+    print("-----------------")
+    print("GETTING USERNAME / AUTHOR NAME MAPPINGS...")
+    print("-----------------")
+
+    users_authors = read_author_names()
+
+    print("-----------------")
+    print("CHECKING VERSION HISTORIES...")
+    print("-----------------")
 
     histories = []
 
     repos_dirpath = os.path.abspath(os.path.join(os.path.dirname(__file__), "..", "repos")) # need absolute path to pass to os.listdir(), adapted from source: https://stackoverflow.com/a/2860193/670433
     repos = os.listdir(repos_dirpath)
-
+    repos.sort()
     for repo in repos:
-        print("-----------------")
-        print(repo.upper())
-        print("-----------------")
+        print(" ... ", repo)
         repo_path = os.path.join(os.path.dirname(__file__), "..", "repos", repo)
-        #print(repo_path)
         os.chdir(repo_path)
 
-        # source: https://stackoverflow.com/questions/677436/how-do-i-get-the-git-commit-count
-
-        #command = f"git rev-list --count master" #> 36
-        #command = f"git shortlog -s -n"
-        #> 24 Author XYZ
-        #> 12 Author123
-        command = f"git shortlog -s -e"
-        #> 24 Author XYZ <xyz@Username-MacBook-Pro.local>
-        #> 12 Author123 <author123@users.noreply.github.com>
-
+        #command = f"git shortlog -s -e"
+        ##> 24     Author XYZ <xyz@Username-MacBook-Pro.local>
+        ##> 12     Author123 <author123@users.noreply.github.com>
+        command = f"git shortlog -s"
+        #> 24     Author XYZ
+        #> 12     Author123
         cmd_results, cmd_err = system_command(command)
         if cmd_err:
             print("ERROR:", cmd_err)
             exit()
         if cmd_results:
             parsed_results = parsed_output(cmd_results)
-            #> '36\tMYUSERNAME'
-            #> '24\tAuthor XYZ\n     12\tAuthor123'
-
-            #> '36\tMYUSERNAME <email>'
-
-            per_author = parsed_results.split("\n")
-            per_author = [a.strip() for a in per_author] #> ['24\tAuthor XYZ', '12\tAuthor123']
-            for s in per_author:
-                author_commits = s.split("\t") #> ['36', 'MYUSERNAME']
+            author_results = parsed_results.split("\n")
+            #author_results = [s.strip() for s in author_results]
+            #> ['24\tAuthor XYZ <email>', '12\tAuthor123 <email>']
+            for s in author_results:
+                author_commits = s.split("\t") #> ['24', 'Author XYZ <email>']
                 commit_count = int(author_commits[0])
-                author = author_commits[1]
-                author = author.split("<")
-                author_name = author[0].strip()
-                author_email = author[1].replace(">","").strip()
-                if not author_name in excluded_author_names:
-                    histories.append({
-                        "owner": repo,
-                        "author_name": author_name,
-                        "author_email": author_email,
-                        "commit_count": commit_count
-                    })
+                author = author_commits[1] #> 'Author XYZ <email>'
+                #author = author.split("<")
+                #author_name = author[0].strip()
+                #author_email = author[1].replace(">","").strip()
 
-    write_results_to_file(results=histories)
+                author_name = author
+                if not author_name in excluded_author_names:
+                    history = {
+                        "owner": repo,
+                        "username": lookup_username(author_name, users_authors),
+                        "author_name": author_name,
+                        #"author_email": author_email,
+                        "commit_count": commit_count
+                    }
+                    #print(history)
+                    histories.append(history)
+
+    username_histories = group_by_username(histories)
+
+    write_results_to_file(results=username_histories)
