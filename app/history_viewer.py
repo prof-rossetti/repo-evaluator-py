4c7307909f81a231044da7b21e703503caaf24ce
Mike Rossetti <s2t2@users.noreply.github.com>
2019-02-17 22:16:31 -0500

    WIP - module import error

--- app/history_viewer.py
+++ app/history_viewer.py
@@ -1,32 +1,43 @@
+import os
 import webbrowser
 
-from app.repo_downloader import read_submission_from_file
+#from app.repo_downloader import read_submission_from_file # pytest likes this
+from repo_downloader import read_submission_from_file # python likes this
 
-# Different URL templates for commit history pages:
+# SUBMISSION URL EXAMPLES:
+# ... https://github.com/{USERNAME}/{REPONAME}
+# ... https://github.com/{USERNAME}/{REPONAME}/blob/master/{FILENAME}.py
+#
+# HISTORY URLS:
 # ... https://github.com/{USERNAME}/{REPONAME}/commits/master"
 # ... https://github.com/{USERNAME}/{REPONAME}/commits/master/{FILENAME}.py"
-# Some submission URLs may include the file paths already, but most might not.
-# TODO: prefer file-specific history, if possible
+#
 def history_url(submission_url):
-    return f"{submission_url}/commits/master" # view the history for whatever url (repo or file) was submitted
+    if "/blob/" in submission_url:
+        return submission_url.replace("blob", "commits")
+    else:
+        return os.path.join(submission_url, "commits", "master")
 
 if __name__ == "__main__":
+
     submissions = read_submission_from_file("db/submissions.csv")
     urls = [history_url(s["repository_url"]) for s in submissions]
 
     print("-----------------")
     confirm = input(f"DETECTED {len(urls)} HISTORY URLS. \nCONTINUE TO OPEN THEM ALL IN A BROWSER? (Y/N): ")
 
-    if confirm.upper() == "Y":
-        for url in urls:
-            print("---")
-            print("OPENING...")
-            print(url)
-            # see: https://docs.python.org/3/library/webbrowser.html
-            # open_new_tab() defaults to open_new() if necessary
-            webbrowser.open_new_tab(url)
-        print("-----------------")
-        print("DONE. HAPPY GRADING!")
-
+    if confirm.upper() != "Y":
+        exit("CANCELLING... PLEASE TRY AGAIN.")
     else:
-        print("CANCELLING... PLEASE TRY AGAIN.")
+        for url in urls:
+            print(f" ... {url}") # source: https://docs.python.org/3/library/webbrowser.html
+            #webbrowser.open_new_tab(url)
+        print("-----------------")
+
+    print("DONE. HAPPY GRADING!")
+
+
+
+    # TODO: consider additional functionality leveraging the git CLI to:
+    # ... analyze the histories / count the commits
+    # ... write results to CSV file
--- tests/history_viewer_test.py
+++ tests/history_viewer_test.py
@@ -1,11 +1,11 @@
 from app.history_viewer import history_url
 
 def test_repo_history_url():
-    repo_url = "__________"
-    expected_url = "________"
+    repo_url = "https://github.com/user123/my-project"
+    expected_url = "https://github.com/user123/my-project/commits/master"
     assert history_url(repo_url) == expected_url
 
 def test_file_history_url():
-    file_url = "__________"
-    expected_url = "________"
+    file_url = "https://github.com/user123/my-project/blob/master/my_script.py"
+    expected_url = "https://github.com/user123/my-project/commits/master/my_script.py"
     assert history_url(file_url) == expected_url
