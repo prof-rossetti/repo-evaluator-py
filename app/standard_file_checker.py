0f739bc53fec0eb8f6abe2398ba137c89146a01f
Mike Rossetti <s2t2@users.noreply.github.com>
2019-07-07 16:21:33 -0400

    Check for tests; closes #5

--- app/standard_file_checker.py
+++ app/standard_file_checker.py
@@ -1,30 +1,23 @@
-#import csv
 import os
 from pathlib import Path
 
-TEST_FILE_SUBSTR= os.getenv("TEST_FILE_SUBSTR", default="test") # can be "spec", etc.
-REPOS_DIRPATH = os.path.abspath(os.path.join(os.path.dirname(__file__), "..", "repos"))
+import pandas
 
-#CSV_FILEPATH = os.path.abspath(os.path.join(os.path.dirname(__file__), "..", "reports", "standard_files_checked.csv"))
+TEST_FILE_SUBSTR= os.getenv("TEST_FILE_SUBSTR", default="test") # can be "spec", etc.
+
+REPOS_DIRPATH = os.path.join(os.path.dirname(__file__), "..", "repos")
+CSV_FILEPATH = os.path.join(os.path.dirname(__file__), "..", "reports", "standard_files_checked.csv")
 
 if __name__ == "__main__":
 
     repo_names = os.listdir(REPOS_DIRPATH)
 
+    results = []
     for repo_name in repo_names:
-        print("-----------------")
-        print(repo_name.upper())
-        print("-----------------")
+        #print("-----------------")
+        #print(repo_name.upper())
+        #print("-----------------")
         repo_path = os.path.join(REPOS_DIRPATH, repo_name)
-
-        #tests_present = False
-        #python_files = Path(repo_path).glob("**/*.py")
-        #for python_filename in python_files:
-        #    #print("...", python_filename)
-        #    #type(python_filename) #> PosixPath
-        #    if "_test" in str(python_filename) or "test_" in str(python_filename):
-        #        tests_present = True
-        #print("TESTS PRESENT:", tests)
 
         readme_present = False
         license_present = False
@@ -32,8 +25,6 @@ if __name__ == "__main__":
 
         all_files = Path(repo_path).glob("**/*")
         for filepath in all_files:
-            #filename = str(filepath)
-            #filename = os.path.split(filename)[-1]
             filestem = filepath.stem #> LICENSE
             if filestem in ["README", "README.md", "README.txt"]:
                 readme_present = True
@@ -42,8 +33,19 @@ if __name__ == "__main__":
             elif f"_{TEST_FILE_SUBSTR}" in filestem or f"{TEST_FILE_SUBSTR}_" in filestem:
                 tests_present = True
 
-        #print("README PRESENT:", readme_present)
-        #print("LICENSE PRESENT:", license_present)
-        #print("TESTS PRESENT:", tests_present)
-        results = {"README": readme_present, "LICENSE": license_present, "TESTS": tests_present}
-        print(results)
+        result = {
+            "REPO": repo_name,
+            "README": readme_present,
+            "LICENSE": license_present,
+            "TESTS": tests_present
+        }
+        print(result)
+        results.append(result)
+
+
+    print("-------------")
+    print("WRITING TO CSV", CSV_FILEPATH)
+    print("-------------")
+
+    df = pandas.DataFrame(results)
+    df.to_csv(CSV_FILEPATH, index=False)
