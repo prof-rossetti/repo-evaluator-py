d04b1a1c906a229bce4498b2fe9dcafe66d28f7e
Mike Rossetti <s2t2@users.noreply.github.com>
2019-07-07 15:58:55 -0400

    Loop through python files

--- app/standard_file_checker.py
+++ app/standard_file_checker.py
@@ -1,32 +1,22 @@
 #import csv
 import os
+from pathlib import Path
 
-REPOS_DIRPATH = os.path.abspath(os.path.join(os.path.dirname(__file__), "..", "repos")) # need absolute path to pass to os.listdir(), adapted from source: https://stackoverflow.com/a/2860193/670433
-
-#STANDARD_FILES = {
-#    "README": ["README", "README.md", "README.txt"],
-#    "LICENSE": ["LICENSE", "LICENSE.md", "LICENSE.txt"]
-#}
+REPOS_DIRPATH = os.path.abspath(os.path.join(os.path.dirname(__file__), "..", "repos"))
 
 #CSV_FILEPATH = os.path.abspath(os.path.join(os.path.dirname(__file__), "..", "reports", "standard_files_checked.csv"))
 
 if __name__ == "__main__":
 
-    repos = os.listdir(REPOS_DIRPATH)
+    repo_names = os.listdir(REPOS_DIRPATH)
 
-    #results = []
-    for repo_name in repos:
+    for repo_name in repo_names:
         print("-----------------")
         print(repo_name.upper())
         print("-----------------")
         repo_path = os.path.join(REPOS_DIRPATH, repo_name)
-        print(repo_path)
 
-        #results.append()
-
-    #print("-----------------")
-#
-    #if results:
-    #    #write_results_to_file(results=results, filename="db/files_checked.csv")
-    #else:
-    #    print("OOPS, NO REPOS. HAVE YOU RUN THE REPO DOWNLOADER?")
+        # h/t: https://stackoverflow.com/a/2186565/670433
+        python_files = Path(repo_path).glob("**/*.py")
+        for python_filename in python_files:
+            print("...", python_filename)
